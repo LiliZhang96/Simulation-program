912f144ea560edf06ff6e0ce999427d77807dc71
LiliZhang96 <102896075+LiliZhang96@users.noreply.github.com>
2022-04-15 10:10:51 +0800

    Update commodel1.m

--- commodel1.m
+++ commodel1.m
@@ -1,6 +1,6 @@
 function dx=commodel1(t,x,flag)
 
-[dx,x_11,x_12,x_21,x_22,x_31,x_32,x_41,x_42,s_11,s_21,s_31,s_41]=comcontrol1(t,x);
+[dx,x_11,x_12,x_21,x_22,x_31,x_32,x_41,x_42,s_11,s_21,s_31,s_41,u_1,u_2,u_3,u_4]=comcontrol1(t,x);
 
 
 
